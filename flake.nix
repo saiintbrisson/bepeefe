2249cf269979a4f9fbe9e59f137903fa780d88c9
Luiz Carvalho <luizcmpc@gmail.com>
2025-10-12 18:39:53 -0300

    chore: add nix file

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            pkg-config
+            just
+            git
+
+            llvmPackages_19.libllvm
+            llvmPackages_19.stdenv
+            llvmPackages_19.libcxx
+            llvmPackages_19.libcxxStdenv
+            llvmPackages_19.clang-unwrapped
+          ];
+
+          shellHook = ''
+            export C_INCLUDE_PATH="${pkgs.llvmPackages_19.clang-unwrapped.lib}/lib/clang/19/include"
+            export CPLUS_INCLUDE_PATH="${pkgs.llvmPackages_19.libcxx.dev}/include/c++/v1:${pkgs.llvmPackages_19.clang-unwrapped.lib}/lib/clang/19/include"
+
+            export PATH="${pkgs.llvmPackages_19.clang-unwrapped}/bin:$PATH"
+            echo "Using clang version: $(clang --version | head -n1)"
+          '';
+        };
+      }
+    );
+}
--- justfile
+++ justfile
@@ -2,13 +2,13 @@ run in="bpf.c" out="bpf.o" program="entry": (build in out)
     cargo r -- {{out}} {{program}}
 
 build in="bpf.c" out="bpf.o":
-    clang-18 -O2 -g -o {{out}} -target bpf -c {{in}}
+    clang-19 -O2 -g -o {{out}} -target bpf -c {{in}}
 
 c2asm in="bpf.c" out="bpf.s":
-    clang-18 -S -o {{out}} -target bpf -c {{in}}
+    clang-19 -S -o {{out}} -target bpf -c {{in}}
 
 asm2o in="bpf.s" out="bpf.o":
-    llvm-mc-18 -triple bpf -filetype=obj -o {{out}} {{in}}
+    llvm-mc-19 -triple bpf -filetype=obj -o {{out}} {{in}}
 
 dump in="bpf.c" out="bpf.o": (build in out)
-    llvm-objdump-18 -dr {{out}}
+    llvm-objdump-19 -dr {{out}}
